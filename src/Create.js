4ffe101f2d62fa6a5f50b8bb6ceb8a3d10764846
ShubhangiMalik <55026486+ShubhangiMalik@users.noreply.github.com>
2021-04-25 23:33:42 +0530

    Project complete

---
+++ src/Create.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+const Create = () => {
+   
+    const [ title, setTitle ] = useState('');
+    const [ body, setBody ] = useState('');
+    const [ author, setAuthor ] = useState('');
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory()
+
+    const handleSubmit =(e) => {
+        e.preventDefault();
+        const blog = { title, body, author }
+        setIsPending(true)
+        fetch('http://localhost:8000/blogs/' , {
+            method : 'POST' ,
+            headers : { 'Content-type' : 'application/json' },
+            body : JSON.stringify(blog)
+
+        }).then( () => {
+            history.push('/');
+        }) 
+        setIsPending(false)
+    }
+
+    return ( 
+        <div className="create">
+            
+            <h2>Add a New Blog</h2>
+            <form onSubmit = {handleSubmit}>
+                <label> Blog Title: </label>
+                <input 
+                type = "text"
+                required
+                value = {title}
+                onChange = { (e) => setTitle(e.target.value )}
+                />
+                <label>Blog body: </label>
+                <textarea 
+                 rows='4'
+                 required
+                 value = {body}
+                 onChange = { (e) => setBody(e.target.value )}
+                 >
+                </textarea>
+                <label>Blog Author: </label>
+                <input
+                 required
+                 value={author}
+                 onChange = { (e) => setAuthor(e.target.value)}
+                />
+                
+               { !isPending && <button>Add Blog</button> } 
+               { isPending && <button disabled>Adding Blog...</button> } 
+            
+            </form>
+        </div>  
+        
+     );
+}
+ 
+export default Create;
